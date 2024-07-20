9be951db7dfd4117405e980228e19bd7b98cf581
BrainCellCoder <00abhisheksharma1999@gmail.com>
2024-07-20 15:03:27 +0530

    Abhishek Sharma Contest

--- script.js
+++ script.js
@@ -8,20 +8,41 @@ let arr = [
 
 function PrintDeveloperbyMap() {
   //Write your code here , just console.log
+  arr.map((data) => {
+    if (data.profession === "developer") console.log(data);
+  });
 }
 
 function PrintDeveloperbyForEach() {
   //Write your code here , just console.log
+  arr.forEach((data) => {
+    if (data.profession == "developer") console.log(data);
+  });
 }
 
 function addData() {
   //Write your code here, just console.log
+  arr.push({ id: 4, name: "susan", age: "20", profession: "intern" });
+  arr.forEach((data) => {
+    console.log(data);
+  });
 }
 
 function removeAdmin() {
   //Write your code here, just console.log
+  let filteredArr = arr.filter((data) => data.profession !== "admin");
+  console.log(filteredArr);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  let arr2 = [
+    { id: 4, name: "Priyanka", age: "28", profession: "Teacher" },
+    { id: 5, name: "Deepsikha", age: "23", profession: "Student" },
+    { id: 6, name: "Kriti", age: "22", profession: "Swimmer" },
+  ];
+
+  let result = arr.concat(arr2);
+
+  console.log(result);
 }
